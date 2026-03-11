3ec03dacd4e479f960777540ccdd14747b0eee94
Ruslan Usmanov <ruslanusm6@gmail.com>
2026-03-12 01:12:47 +0300

    fix docker compose

--- products-api/pkg/api/router.go
+++ products-api/pkg/api/router.go
@@ -8,11 +8,7 @@ import (
 
 	"market/products-api/pkg/middleware"
 
-	docs "market/products-api/docs"
-
 	"github.com/gin-gonic/gin"
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"go.uber.org/zap"
 	"golang.org/x/time/rate"
 )
@@ -38,14 +34,14 @@ func NewRouter(logger *zap.Logger, db database.Database, redisClient cache.Cache
 	r.Use(middleware.Cors())
 	r.Use(middleware.RateLimiter(rate.Every(1*time.Minute), 60))
 
-	docs.SwaggerInfo.BasePath = "/api/v1"
+	// docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/", bookRepository.Healthcheck)
 		v1.GET("/books", middleware.APIKeyAuth(), bookRepository.FindBooks)
 		// v1.POST("/books", middleware.APIKeyAuth(), middleware.JWTAuth(), bookRepository.CreateBook)
 	}
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	return r
 }
